3e71dba28d1e83dc861fb36be03ef04ef211126a
Pamidonte <francescomarrone8@gmail.com>
2022-11-14 15:44:38 +0100

    jsnacks2

---
+++ jsnacks2/script.js
@@ -0,0 +1,23 @@
+let primaparola = prompt("inserisci una parola");
+
+let secondaparola = prompt("inserisci un altra parola");
+
+if (primaparola.length > secondaparola.length){
+    console.log( secondaparola + "," + primaparola);
+
+}else if (secondaparola.length > primaparola.length){
+    console.log(primaparola + "," + secondaparola);
+
+}
+
+
+    
+
+
+
+
+
+
+
+
+
