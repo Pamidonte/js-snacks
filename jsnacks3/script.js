e2eb47c828dc578253ac508051a307cdabacfb59
Pamidonte <francescomarrone8@gmail.com>
2022-11-14 16:05:36 +0100

    jsnacks3

---
+++ jsnacks3/script.js
@@ -0,0 +1,19 @@
+let array = [];
+let somma = 0;
+
+for (let i = 0; i < 10; i++){
+    let numero = prompt("scrivi un numero");
+    numero = parseInt(numero);
+    array.push(numero);
+    somma = somma + numero;
+};
+console.log(somma);
+
+
+
+
+
+
+
+
+
