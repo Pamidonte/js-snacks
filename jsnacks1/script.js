d621795c45ce831c0d4235a16e1a6c3a28f9259f
Pamidonte <francescomarrone8@gmail.com>
2022-11-14 14:38:54 +0100

    jsnack1

---
+++ jsnacks1/script.js
@@ -0,0 +1,25 @@
+let primonumero = prompt("inserisci un numero");
+
+let secondonumero = prompt("inserisci un altro numero");
+
+
+
+if (primonumero > secondonumero){
+    console.log(primonumero);
+    
+} else if (secondonumero > primonumero){
+    console.log(secondonumero);
+    
+} else {
+    console.log("pari e patta");
+  }
+    
+
+
+
+
+
+
+
+
+
